cfb0a8591a45444520deb198a8fa8e9bf4ab925a
Sergei Lukashin <lsv1387@gmail.com>
2022-10-07 17:53:22 +0600

    module_3_on_review

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,20 @@
 function getMinMax(str) {
-  // ваш код...
+  let resObj = {
+    min: null,
+    max: null
+  }
+  let numArr = str.split(' ').filter(item => (!Number.isNaN(parseFloat(item)))).map(number => parseFloat(number))
+  let minValue = numArr[0];
+  let maxValue = numArr[0];
+  for (let i = 0; i < numArr.length; i++) {
+    if (minValue > numArr[i]) {
+      minValue = numArr[i]
+    }
+    if (maxValue < numArr[i]) {
+      maxValue = numArr[i]
+    }
+  }
+  resObj.min = minValue
+  resObj.max = maxValue
+  return resObj
 }
